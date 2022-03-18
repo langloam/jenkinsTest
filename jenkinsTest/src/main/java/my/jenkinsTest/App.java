c433f9b8f4a72016b6805e146532d1db1d2f5b30
gordon <gordon.lansheu@cloudysys.com>
2022-03-18 16:45:12 +0800

    something broken

--- jenkinsTest/src/main/java/my/jenkinsTest/App.java
+++ jenkinsTest/src/main/java/my/jenkinsTest/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Look at this beautiful line!" );
         System.out.println( "Look at this beautiful line too!" );
+        System.out.println( "Look at this beautiful line ? );
     }
 }
